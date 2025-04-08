ea00fbd7a460f8601c6000b26619e85814353270
xerottin <xerottin@gmail.com>
2025-04-08 09:25:19 +0500

    xuyeta

--- README.md
+++
@@ -1,1 +0,0 @@
-# imporove_2
--- leetcode.py
+++ leetcode.py
@@ -88,19 +88,19 @@ from typing import List
 
 #4
 
-class Solution:
-    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        length = len(flowerbed)
-        for i in range(length):
-            if flowerbed[i] == 0:
-                left_empty = i == 0 or flowerbed[i - 1] == 0
-                right_empty = i == length - 1 or flowerbed[i + 1] == 0
-                if left_empty and right_empty:
-                    flowerbed[i] = 1
-                    n-=1
-                if n == 0:
-                    return True
-        return n<=0
+# class Solution:
+#     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+#         length = len(flowerbed)
+#         for i in range(length):
+#             if flowerbed[i] == 0:
+#                 left_empty = i == 0 or flowerbed[i - 1] == 0
+#                 right_empty = i == length - 1 or flowerbed[i + 1] == 0
+#                 if left_empty and right_empty:
+#                     flowerbed[i] = 1
+#                     n-=1
+#                 if n == 0:
+#                     return True
+#         return n<=0
 
 
 
