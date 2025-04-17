1d4c52a1bd8cd280e6c804116c41e60edd29761a
xerottin <xerottin@gmail.com>
2025-04-17 10:11:16 +0500

    lit test

--- main.py
+++ main.py
@@ -37,29 +37,33 @@
 # print(add_numbers())
 
 
-class User(ABC):
-    @abstractmethod
-    def get_permision(self):
-        pass
+# class User(ABC):
+#     @abstractmethod
+#     def get_permision(self):
+#         pass
+#
+#
+# class Admin(User):
+#     def get_permision(self):
+#         return ["read", "write", "delete"]
+#
+# class RegularUser(User):
+#     def get_permision(self):
+#         return ["read"]
+#
+# def get_current_user(is_admin: bool) -> User:
+#     if is_admin:
+#         return Admin()
+#     return RegularUser()
+#
+# @app.get("/permission")
+# def read_permision(user: User= Depends(get_current_user)):
+#     return {"permision": user.get_permision()}
 
 
-class Admin(User):
-    def get_permision(self):
-        return ["read", "write", "delete"]
+def add_numbers(*args):
+    return sum(args)
 
-class RegularUser(User):
-    def get_permision(self):
-        return ["read"]
-
-def get_current_user(is_admin: bool) -> User:
-    if is_admin:
-        return Admin()
-    return RegularUser()
-
-@app.get("/permission")
-def read_permision(user: User= Depends(get_current_user)):
-    return {"permision": user.get_permision()}
-
-
-
+print(add_numbers(1, 2, 3, 4))
+print(add_numbers())
 
